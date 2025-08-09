0bcd63ff2a8cc09a6417ff2beccfe7a44f4d4b35
SuleymanDD <sdemirel.2375@gmail.com>
2025-08-09 22:03:38 +0300

    Odev_7 file added

---
+++ Odev_7.sql
@@ -0,0 +1,23 @@
+-- The answers to assignment 7 are given below.
+
+-- First Quest -> Group the movies in the movie table according to their rating values.
+SELECT rating, COUNT(*) FROM film GROUP BY rating;
+
+-- Second Quest -> When we group the movies in the movie table according to the replacement_cost column, sort the replacement_cost value that has more than 50 movies and the corresponding number of movies.
+SELECT replacement_cost, COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING COUNT(*) > 50;
+
+-- Third Quest -> What are the customer numbers corresponding to the store_id values in the customer table?
+SELECT store_id, COUNT(*) FROM customer GROUP BY store_id;
+
+-- Fourth Quest -> After grouping the city data in the city table according to the country_id column, share the country_id information and the number of cities that contain the highest number of cities.
+SELECT country_id, COUNT(*) FROM city
+GROUP BY country_id
+ORDER BY COUNT(*) DESC
+LIMIT 1;
+
+
+
+
+
