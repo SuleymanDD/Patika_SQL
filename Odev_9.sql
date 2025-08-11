64833b295afaa3806959dec7be85c7e1d76a2b67
SuleymanDD <sdemirel.2375@gmail.com>
2025-08-11 21:23:50 +0300

    Odev_9 file added

---
+++ Odev_9.sql
@@ -0,0 +1,26 @@
+-- The answers to assignment 9 are given below.
+
+-- First Quest -> Write an INNER JOIN query that will allow us to see the city and country names in the city table and country table together.
+/*
+SELECT * FROM city
+INNER JOIN country ON city.country_id = country.country_id;
+*/
+
+-- Second Quest -> Write an INNER JOIN query where we can see the payment_id in the customer table and payment table, as well as the first_name and last_name names in the customer table.
+/*
+SELECT payment.payment_id, customer.first_name, customer.last_name FROM customer
+INNER JOIN payment ON customer.customer_id = payment.customer_id;
+*/
+
+-- Third Quest -> Write an INNER JOIN query that will allow us to see the rental_id in the customer table and the rental table, as well as the first_name and last_name names in the customer table.
+/*
+SELECT rental.rental_id, customer.first_name, customer.last_name FROM customer
+INNER JOIN rental ON customer.customer_id = rental.customer_id;
+*/
+
+
+
+
+
+
+
